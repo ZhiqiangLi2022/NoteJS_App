39b1bf12c6ee41d1110948b38c6098062699deb7
ZhiqiangLi <lizhiqiang060@gmail.com>
2023-02-05 11:45:59 -0600

    Added some test changes.

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<header><title> My First Page </title></header>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1><p>some text</p></body>');
     res.write('</html>');
 });
 
