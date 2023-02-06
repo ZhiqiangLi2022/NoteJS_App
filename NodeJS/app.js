1e6740233729a127cf8fe5241fe4b5347df5d77d
ZhiqiangLi <lizhiqiang060@gmail.com>
2023-02-06 11:37:08 -0600

    #03-clone operation update

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -7,16 +7,16 @@ const server = http.createServer((req, res) => {
     if(url === '/'){
         res.write('<html>');
         res.write('<header><title>Enter Message</title></header>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><buttom type="submit">Send</buttom></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><buttom type="submit">Send</buttom></input></form></body>');
         res.write('</html>');
         return res.end();
     }
-    // res.setHeader('Content-Type', 'text/html');
-    // res.write('<html>');
-    // res.write('<header><title> My First Page </title></header>');
-    // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    // res.write('</html>');
-    // res.end();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<header><title> My First Page </title></header>');
+    res.write('<body><h1>God, damn it</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
-server.listen(2000);
+server.listen(3000);
