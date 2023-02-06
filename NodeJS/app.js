3bf24ece6149967f62f08b3637525871a996e2db
ZhiqiangLi <lizhiqiang060@gmail.com>
2023-02-06 10:55:51 -0600

    #02-change for url message

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,13 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
     //process.exit();
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<header><title> My First Page </title></header>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1><p>some text</p></body>');
-    res.write('</html>');
+    const url = req.url;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<header><title>Enter Message</title></header>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><buttom type="submit">Send</buttom></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+    // res.write('<header><title> My First Page </title></header>');
+    // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    // res.write('</html>');
+    // res.end();
 });
 
-server.listen(3000);
+server.listen(2000);
